064b58c6af1714567f4fb5024a630a713e995bb4
sahil1si18ec083 <103936307+sahil1si18ec083@users.noreply.github.com>
2024-05-31 18:37:25 +0530

    Replaced local database with online DB

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const express = require("express");
 const app = express();
 const chalk = require("chalk");
 const personRoutes = require("./router/personRoutes.js");
+require("dotenv").config();
 
-const dotenv = require("dotenv").config()
-console.log(dotenv)
 app.use(bodyParser.json());
 const myMiddlewareRequestLogging = function (req, res, next) {
   console.log(chalk.blue("Hello world!"));
   console.log(new Date().toLocaleString());
   next();
 };
-const url = "mongodb://127.0.0.1:27017/helloworld";
+// const url = "mongodb://127.0.0.1:27017/helloworld";
+const url = process.env.DB_URL;
+const port = process.env.PORT || 8080;
 
 connectToDB(url);
 
 app.use("/person", myMiddlewareRequestLogging, personRoutes);
 
-app.listen(8080);
+app.listen(port);
